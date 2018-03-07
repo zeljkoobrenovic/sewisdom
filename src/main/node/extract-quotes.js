7afa6423d32b9c4a9daf3dc78454029d9fd4c6b4
zeljkoobrenovic <zeljkoobrenovic@yahoo.com>
2018-03-07 15:07:46 +0100

    init

--- src/main/node/extract-quotes.js
+++ src/main/node/extract-quotes.js
@@ -21,7 +21,7 @@ fs.readFile('quotes.json', 'utf8', function (err, contents) {
 
         var cover = year + "-" + number;
         if (prevCover !== cover) {
-            text += "\n\n* * *\n\n![](images/" + cover + ".jpg)\n\n* * *\n\n\n{pagebreak}\n\n";
+            text += "\n\n* * *\n\n![](images/" + cover + ".jpg)\n* * *\n\n\n{pagebreak}\n\n";
             prevCover = cover;
         }
 
@@ -30,7 +30,8 @@ fs.readFile('quotes.json', 'utf8', function (err, contents) {
         quote = quote.replace(/<\/b>/g, "**");
 
         quote = "  \n* * *\n\n\"" + quote + "\"";
-        quote += "  \n\n* * *\n\n***" + q.citation.replace(/<.*?>/g, "") + "***\n\n";
+        quote += "  \n\n* * *\n\n***" + q.citation.replace(/<.*?>/g, "") + "***[^foo" + i + "]\n\n";
+        quote += "[^foo" + i + "]: " + q.doiLink;
 
         quote += "\n";
 
