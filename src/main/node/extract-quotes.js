854c312362986d751374474a2a273d635d546ee4
zeljkoobrenovic <zeljkoobrenovic@yahoo.com>
2018-03-07 14:05:02 +0100

    init

--- src/main/node/extract-quotes.js
+++ src/main/node/extract-quotes.js
@@ -21,7 +21,7 @@ fs.readFile('quotes.json', 'utf8', function (err, contents) {
 
         var cover = year + "-" + number;
         if (prevCover !== cover) {
-            text += "\n \n![](images/" + cover + ".jpg)\n{pagebreak}\n";
+            text += " \n \n![](images/" + cover + ".jpg)\n\n{pagebreak}\n\n\n";
             prevCover = cover;
         }
 
@@ -29,7 +29,7 @@ fs.readFile('quotes.json', 'utf8', function (err, contents) {
         quote = quote.replace(/<b>/g, "**");
         quote = quote.replace(/<\/b>/g, "**");
 
-        quote = "*\"" + quote + "\"*";
+        quote = " \n \n*\"" + quote + "\"*";
         quote += " \n \n**" + q.citation.replace(/<.*?>/g, "") + "**\n\n";
 
         quote += "\n";
