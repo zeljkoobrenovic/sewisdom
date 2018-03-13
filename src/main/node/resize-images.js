7e0932b6f7e670bae50ee31b1dddac07c5753948
zeljkoobrenovic <zeljkoobrenovic@yahoo.com>
2018-03-13 12:31:52 +0100

    init

--- src/main/node/resize-images.js
+++ src/main/node/resize-images.js
@@ -1,17 +1,22 @@
 const fs = require('fs');
-const sharp = require('sharp');
+const lwip = require('lwip');
 
 var dirSrc = '../../../manuscript/images-high-quality/';
 var dirDest = '../../../manuscript/images-medium-quality/';
 
 fs.readdir(dirSrc, function(err, files) {
     files.forEach(function (file) {
-        sharp(dirSrc + file)
-            .resize(undefined, 600)
-            .toFile(dirDest + file, function (err) {
-                if (err) console.log(err);
-            });
-    });
+        var srcImage = dirSrc + file;
+        var destImage = dirDest + file;
+        lwip.open(srcImage, function(err, image){
+            if (err) return console.log(err);
+            image.batch()
+                .scale(0.75)
+                .writeFile(destImage, function(err){
+                    if (err) console.log(err);
+                });
+
+        });    });
 });
 
 
